0498ec6e3d2fc9d8b2c8683b2b4622675addb3dc
ikedachin <95423050+ikedachin@users.noreply.github.com>
2024-08-26 11:54:12 +0900

    監視は漢字入力でしか機能しなかったのでセットボタンを復活

--- static/script/timer.js
+++ static/script/timer.js
@@ -8,54 +8,64 @@ const app = Vue.createApp({
         countDowntime: '未設定', // 表示内容
         setTimeIsVisible: false, // 表示切替用フラグ
         flagInCount: false, // 二重でcountStartを実行した時にキャンセルするためのフラグ
-        // alart: '', // 時間を入力しなかった時にアラートを出すため
-        
+        alart: '', // 時間を入力しなかった時にアラートを出すため
+        resetIsVisible: false,
     }),
     mounted() {
         // this.countStart();
         // setInterval(this.countStart, 1000)
     },
-    watch: {
-        setTimeString: function(value) {
-            if (value !== "時間が終了しました！" & value !== '') {
-
-                
-                value = value.replace('：', ':')
-                value = value.replace(/[０-９]/g, function(s) {
-                        return String.fromCharCode(s.charCodeAt(0) - 0xFEE0);
-                    });
-                let strMin = value.split(':')[0];
-                let strSec = value.split(':')[1];
-                    
+    // watch: { // 上手くいかなかったので一旦廃止
+    //     setTimeString: function(value) {
+    //         if (value !== "時間が終了しました！") {
+    //             value = value.replace('：', ':')
+    //             value = value.replace(/[０-９]/g, function(s) {
+    //                 return String.fromCharCode(s.charCodeAt(0) - 0xFEE0);
+    //             });
+    //             if (value.includes(':')){
+    //                 try {
+    //                     // if (value !== "時間が終了しました！" & value !== '') {
+    //                         let strMin = value.split(':')[0];
+    //                         let strSec = value.split(':')[1];
+    //                         this.setSeconds = parseInt(strMin, 10) * 60 + parseInt(strSec);
+    //                         let minutes = Math.floor(this.setSeconds / 60);
+    //                         let seconds = this.setSeconds % 60;
+    //                         this.countDowntime = `${minutes.toString().padStart(2, '0')}分${seconds.toString().padStart(2, '0')}秒`;
+    //                         this.setTimeIsVisible = true;
+    //                     } catch (err) {
+    //                         console.log(err)
+    //                         this.setTimeIsVisible = false;
+    //                     }
+    //             }
+    //         }
+    //     }
+    // },
+    methods: {
+        set: function (event) {
+            if (this.setTimeString === '') {
+                this.alart = '"mm:ss"の形式で時間を設定してください'
+            } else {
+                this.setTimeString = this.setTimeString.replace('：', ':')
+                this.setTimeString = this.setTimeString.replace(/[０-９]/g, function(s) {
+                    return String.fromCharCode(s.charCodeAt(0) - 0xFEE0);
+                });
+                let strMin = this.setTimeString.split(':')[0];
+                let strSec = this.setTimeString.split(':')[1];
                 this.setSeconds = parseInt(strMin, 10) * 60 + parseInt(strSec);
-                
                 let minutes = Math.floor(this.setSeconds / 60);
                 let seconds = this.setSeconds % 60;
-                this.countDowntime = `${minutes.toString().padStart(2, '0')}分${seconds.toString().padStart(2, '0')}秒`;
-                this.setTimeIsVisible = true;
+                this.countDowntime = 
+                `${minutes.toString().padStart(2, '0')}分${seconds.toString().padStart(2, '0')}秒`;
+                this.setTimeString = this.countDowntime
+                this.setTimeIsVisible = !this.setTimeIsVisible
+                this.resetIsVisible = false
+                this.alart = ''
             }
-    },
-},
-    methods: {
-        // set: function (event) { // 監視プロパティを入れたので廃止
-        //     if (this.setTimeString === '') {
-        //         this.alart = '"mm:ss"の形式で時間を設定してください'
-        //     } else {
-        //         let strMin = this.setTimeString.split(':')[0];
-        //         let strSec = this.setTimeString.split(':')[1];
-        //         this.setSeconds = parseInt(strMin, 10) * 60 + parseInt(strSec);
-        //         let minutes = Math.floor(this.setSeconds / 60);
-        //         let seconds = this.setSeconds % 60;
-        //         this.countDowntime = 
-        //         `${minutes.toString().padStart(2, '0')}分${seconds.toString().padStart(2, '0')}秒`;
-        //         this.setTimeString = this.countDowntime
-        //         this.setTimeIsVisible = !this.setTimeIsVisible
-        //         this.alart = ''
-        //     }
-        // },
+        },
         countStart: function (params) {
             if (this.flagInCount  === false) {
                 this.flagInCount = true;
+                this.resetIsVisible = false
                 countdownInterval = setInterval(() => {    
                     // 時間がゼロになるまでカウントダウン
                     if (this.setSeconds > 0) {
@@ -89,6 +99,7 @@ const app = Vue.createApp({
             try {
                 clearInterval(countdownInterval);
                 this.flagInCount = false;
+                this.resetIsVisible = true;
             } catch (err) {
                 console.log(err);
             }
